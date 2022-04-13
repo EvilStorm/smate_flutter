ab9b1e30584ae06ac660f774b52f7993ee15d799
Min Cheol Kim <mincheolkim@Minui-MacBookPro.local>
2022-04-13 23:31:45 +0900

    added fcm message toast, send fcm token

--- lib/controllers/contoller_user_info.dart
+++ lib/controllers/contoller_user_info.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:get/get.dart';
+import 'package:smate/controllers/basic_controller_fn.dart';
+import 'package:smate/network/http_client.dart';
 
 import '../models/model_user.dart';
 
-class UserInfoController extends GetxController {
+class UserInfoController extends GetxController with BasicControllorFunctions {
   Rx<UserModel> userInfo = UserModel().obs;
 
-  void setUserInfo(UserModel data, bool isSilent) {
+  void setUserInfo(UserModel data, bool isSilent) async {
     userInfo.value = data;
+
+    await sendFcmToken(data.pushToken);
 
     if (isSilent) {
       return;
@@ -19,6 +24,28 @@ class UserInfoController extends GetxController {
       Get.toNamed('/joinProcess');
     } else {
       Get.offAllNamed('/home');
+    }
+  }
+
+  Future<void> sendFcmToken(String? token) async {
+    var currentToken = await FirebaseMessaging.instance.getToken();
+
+    if (token == currentToken) return;
+    await patchUserInfo({'pushToken': currentToken});
+    return;
+  }
+
+  Future<bool> patchUserInfo(dynamic info) async {
+    var response = await HttpClient.instance.patch(
+      '/user',
+      body: info,
+    );
+
+    if (response['code'] == 200) {
+      return true;
+    } else {
+      showMessage(response['data']['message']);
+      return false;
     }
   }
 
